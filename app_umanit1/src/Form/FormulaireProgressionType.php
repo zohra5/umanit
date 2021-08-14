650f813a8664d7d565d475690f4a51303a5eb963
Thomas <thomas-2d@hotmail.fr>
2021-08-14 16:14:16 +0200

    Ajout du twig à la place des inputs html pour ma partie du formulaire progression

--- app_umanit1/src/Form/FormulaireProgressionType.php
+++ app_umanit1/src/Form/FormulaireProgressionType.php
@@ -11,8 +11,9 @@ use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use App\Entity\Activites;
 use App\Entity\EvaluationObjectifs;
 use App\Entity\FormulaireProgression;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -30,18 +31,95 @@ class FormulaireProgressionType extends AbstractType
                 'html5' => true,
                 'widget' => 'single_text'
             ])
-            ->add('ressentiTravail')
+            ->add('ressentiTravail',  ChoiceType::class, 
+            [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    '10' => 10
+                ],
+                'expanded' => true
+            ])
 
-            ->add('aimesTuPoste')
-            ->add('relationsEquipe')
-            ->add('relationsEntreprise')
-            ->add('commentairesQVT')
-            ->add('missionsPrincipales')
-
-            ->add('reussitePoste')
-            ->add('aimesTuFonctions')
-            ->add('pointsAmeliorations')
-            ->add('competencesNonUtilisees')
+            ->add('aimesTuPoste',  ChoiceType::class, 
+            [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    '10' => 10
+                ],
+                'expanded' => true
+            ])
+            ->add('relationsEquipe',  ChoiceType::class, 
+            [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    '10' => 10
+                ],
+                'expanded' => true
+            ])
+            ->add('relationsEntreprise',  ChoiceType::class, 
+            [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                    '6' => 6,
+                    '7' => 7,
+                    '8' => 8,
+                    '9' => 9,
+                    '10' => 10
+                ],
+                'expanded' => true
+            ])
+            ->add('commentairesQVT', TextareaType::class)
+            ->add('missionsPrincipales', TextareaType::class)
+            ->add('activites', CollectionType::class, [
+                'entry_type' => ActivitesType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+            ])
+            ->add('evaluationObjectifs', CollectionType::class, [
+                'entry_type' => EvaluationObjectifsType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+            ])
+            ->add('reussitePoste', TextareaType::class)
+            ->add('aimesTuFonctions', TextareaType::class)
+            ->add('pointsAmeliorations', TextareaType::class)
+            ->add('bilanCompetences', CollectionType::class, [
+                'entry_type' => BilanCompetencesType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+            ])
+            ->add('competencesNonUtilisees', TextareaType::class)
             ->add('objectifDefinition', CollectionType::class, [
                 "entry_type"=>ObjectifDefinitionType::class,
                 "entry_options"=>["label"=>false],
@@ -60,30 +138,9 @@ class FormulaireProgressionType extends AbstractType
                 "allow_add"=>true,
                 "allow_delete"=>true]
                 )/**GAB */
-
-            ->add('activites', CollectionType::class, [
-                'entry_type' => ActivitesType::class,
-                'entry_options' => ['label' => false],
-                'allow_add' => true,
-                'allow_delete' => true,
-            ])
-            ->add('evaluationObjectifs', CollectionType::class, [
-                'entry_type' => EvaluationObjectifsType::class,
-                'entry_options' => ['label' => false],
-                'allow_add' => true,
-                'allow_delete' => true,
-            ])
             ->add('reussitePoste')
             ->add('aimesTuFonctions')
             ->add('pointsAmeliorations')
-            ->add('bilanCompetences', CollectionType::class, [
-                'entry_type' => BilanCompetencesType::class,
-                'entry_options' => ['label' => false],
-                'allow_add' => true,
-                'allow_delete' => true,
-            ])
-            ->add('competencesNonUtilisees')
-
             ->add('remarquesActivites')
             ->add('evalTravail')
             ->add('vieProPerso')
